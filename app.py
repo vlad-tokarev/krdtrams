f541a1b6d57e1587772bdf8880b5d98a03d8aa03
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2019-05-22 06:51:12 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+import requests
+
+import conf
+
+
+def get_timestamp():
+    return int(datetime.utcnow().timestamp() * 1000)
+
+
+def save(data):
+    raise NotImplementedError
+
+
+def fetch():
+
+    current_timestamp = get_timestamp()
+
+    resp = requests.get(f'{conf.TRAM_API_DOMAIN}/krasnodar/gps.txt?{current_timestamp}')
+
+    if resp.status_code == 200:
+        return resp.content
+    else:
+        raise NotImplementedError
+
+
+def run():
+    while True:
+        data = fetch()
+        save(data)
+
+
+if __name__ == '__main__':
+
+    run()
---
+++ conf.py
@@ -0,0 +1,5 @@
+import os
+
+
+TRAM_API_DOMAIN = os.environ.get('TRAM_API_DOMAIN', 'http://m.marsruty.ru')
+FETCH_PERIOD = os.environ.get('FETCH_PERIOD', 5)
